4f4213415fc0e24177b55c7e060ba85f4a6c33d4
Metimer <metinamerwane@gmail.com>
2025-03-19 12:10:18 +0100

    Update extract_classement.py

--- extract_classement.py
+++ extract_classement.py
@@ -14,14 +14,19 @@ ligues = {
     'Angleterre': '9/Statistiques-Premier-League'
 }
 
-navigator = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive'
+}
 
 df_ligues = {}
 
 # Boucle à travers les ligues
 for pays, url_part in ligues.items():
     url = f'https://fbref.com/fr/comps/{url_part}'
-    response = requests.get(url, headers={'User-Agent': navigator})
+    response = requests.get(url, headers=headers)
 
     if response.status_code != 200:
         print(f"❌ Erreur lors de la récupération de la page pour {pays}.")
